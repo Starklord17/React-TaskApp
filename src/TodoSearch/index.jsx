64bfbac372355257a75a4fa0cb1c7a144819b92e
MaxSM <maxi.sm117@gmail.com>
2024-02-01 02:11:51 -0300

    :sparkles: Add search react-icon + styles

--- src/TodoSearch/index.jsx
+++ src/TodoSearch/index.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import "./TodoSearch.css";
 import { TodoContext } from '../TodoContext';
+import { IoMdSearch } from "react-icons/io";
 
 function TodoSearch() {
   const { searchValue, setSearchValue } = React.useContext(TodoContext);
 
-
-  return <input 
-    placeholder="Search" 
-    className="TodoSearch" 
-    value={searchValue}
-    onChange={(event) => {
-      setSearchValue(event.target.value);
-    }}
-  />;
+  return ( 
+    <div className='search-container'>
+      <input 
+        placeholder="Search" 
+        className="TodoSearch" 
+        value={searchValue}
+        onChange={(event) => {
+          setSearchValue(event.target.value);
+        }}
+      />
+      <IoMdSearch className='search-icon' alt='search icon'/>
+    </div>
+  );
 }
 
 export { TodoSearch };
